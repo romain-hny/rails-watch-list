76ef2b0bf03b3086f822988ebe03312b72ffdfae
Romain Heranney <heranney.romain@gmail.com>
2022-11-18 12:18:05 +0100

    add dotenv-rails

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,34 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmarks = Bookmark.where('list_id = ?', @list.id)
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # root to: "lists#index"
+  # get "/lists", to: "lists#index"
+  # get "/lists/new", to: "lists#new"
+  # post "/lists", to: "lists#create"
+  # get "/lists/:id", to: "lists#show", as: "list"
+
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: :destroy
 end
